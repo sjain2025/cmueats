221edd54de5be5f600a7d25bad58a47d1a48091b
Soham Jain <jainsoham01@gmail.com>
2025-09-19 19:17:58 -0400

    Logging latitude and longitude

--- src/components/SortBy.tsx
+++ src/components/SortBy.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import './SortBy.css';
 
 interface SortByProps {
@@ -6,6 +7,24 @@ interface SortByProps {
 }
 
 function SortBy({ setSortBy, sortBy }: SortByProps) {
+    useEffect(() => {
+        if (sortBy === 'location') {
+            if ('geolocation' in navigator) {
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        const { latitude, longitude } = position.coords;
+                        console.log({ latitude, longitude });
+                    },
+                    (error) => {
+                        console.error(error);
+                    }
+                );
+            } else {
+                console.error('error');
+            }
+        }
+    }, [sortBy]);
+
     return (
         <select 
             onChange={(e) => setSortBy(e.target.value)} 
